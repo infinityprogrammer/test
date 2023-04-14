175f450c8caa4cc868ee0f5e69d9df8ed4944a81
INFINITY <infinityprogrammer01@gmail.com>
2023-04-14 18:31:12 +0400

    dev changes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ print("MAde changes in master branch")
 print("Made changes in develop and checkout to main")
 
 def get_develop_name():
+    # made changes in develop
     pass
 
 def get_ful_name():
